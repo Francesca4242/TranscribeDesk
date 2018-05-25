5be516aab1f872c940bbfb226668502bb8d715f9
TranscribeDesker <example@example.com>
2018-05-25 16:57:14 +0100

    Simplify javascript

--- js.js
+++ js.js
@@ -2,11 +2,11 @@ function rgb(r, g, b){
   return "rgb("+r+","+g+","+b+")";
 }
 
-wordDB.map(w => {
+wordDB.forEach(w => {
 	var e = document.createElement('word');
 	e.innerText = w.word;
 	e.style.color = rgb((1-w.accuracy)*255, w.accuracy*255, 0);
 	e.style.left = w.left + 'px';
-    e.style.top = w.top + 'px';
-    return e;
-}).forEach(e => document.getElementById('transcription').appendChild(e));
+  e.style.top = w.top + 'px';
+  document.getElementById('transcription').appendChild(e);
+})
