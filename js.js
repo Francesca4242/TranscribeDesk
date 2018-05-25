7b9979c091ec7b091d747be8920a3ab9e733df3f
TranscribeDesker <example@example.com>
2018-05-25 16:56:16 +0100

    Move word database

--- js.js
+++ js.js
@@ -2,16 +2,6 @@ function rgb(r, g, b){
   return "rgb("+r+","+g+","+b+")";
 }
 
-wordDB = [
-{word: "In", top: 27, left: 10, accuracy: 0.75},
-{word: "the", top: 27, left: 45, accuracy: 0.15},
-{word: "name", top: 27, left: 80, accuracy: 0.25},
-{word: "of", top: 27, left: 120, accuracy: 0.50},
-{word: "God", top: 27, left: 160, accuracy: 0.19},
-{word: "Amen", top: 27, left: 180, accuracy: 0.40},
-{word: "the", top: 27, left: 210, accuracy: 0.90},
-]
-
 wordDB.map(w => {
 	var e = document.createElement('word');
 	e.innerText = w.word;
