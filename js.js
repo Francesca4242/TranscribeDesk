e81a9ec3e648af87d8e1a14329de541d312fe8d1
Francesca4242 <32270681+Francesca4242@users.noreply.github.com>
2018-05-25 16:15:35 +0100

    js database

---
+++ js.js
@@ -0,0 +1,16 @@
+
+
+wordDB = [
+{word: "In", top: 27, left: 10, accuracy: 0.75},
+{word: "the", top: 27, left: 15, accuracy: 0.75},
+{word: "name", top: 27, left: 20, accuracy: 0.75},
+{word: "of", top: 27, left: 30, accuracy: 0.75},
+{word: "God", top: 27, left: 35, accuracy: 0.75},
+{word: "Amen", top: 27, left: 45, accuracy: 0.75},
+{word: "the", top: 27, left: 55, accuracy: 0.75},
+
+
+
+
+
+]
