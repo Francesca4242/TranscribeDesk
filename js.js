5114d9f7057225fec8ae982f0e71b43565255655
Francesca4242 <32270681+Francesca4242@users.noreply.github.com>
2018-05-25 16:41:32 +0100

    changes to positioning and colour of words

--- js.js
+++ js.js
@@ -4,12 +4,12 @@ function rgb(r, g, b){
 
 wordDB = [
 {word: "In", top: 27, left: 10, accuracy: 0.75},
-{word: "the", top: 27, left: 15, accuracy: 0.75},
-{word: "name", top: 27, left: 20, accuracy: 0.75},
-{word: "of", top: 27, left: 30, accuracy: 0.75},
-{word: "God", top: 27, left: 35, accuracy: 0.75},
-{word: "Amen", top: 27, left: 45, accuracy: 0.75},
-{word: "the", top: 27, left: 55, accuracy: 0.75},
+{word: "the", top: 27, left: 45, accuracy: 0.15},
+{word: "name", top: 27, left: 80, accuracy: 0.25},
+{word: "of", top: 27, left: 120, accuracy: 0.50},
+{word: "God", top: 27, left: 160, accuracy: 0.19},
+{word: "Amen", top: 27, left: 180, accuracy: 0.40},
+{word: "the", top: 27, left: 210, accuracy: 0.90},
 ]
 
 wordDB.map(w => {
