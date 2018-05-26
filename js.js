3b5065550855f6bfa029e26ef25ab2a4c95ba69c
TranscribeDesker <example@example.com>
2018-05-26 21:19:25 +0100

    editable words

--- js.js
+++ js.js
@@ -2,11 +2,15 @@ function rgb(r, g, b){
   return "rgb("+r+","+g+","+b+")";
 }
 
+
+
+
 wordDB.forEach(w => {
 	var e = document.createElement('word');
 	e.innerText = w.word;
 	e.style.color = rgb((1-w.accuracy)*255, w.accuracy*255, 0);
 	e.style.left = w.left + 'px';
   e.style.top = w.top + 'px';
+  e.contentEditable = true;
   document.getElementById('transcription').appendChild(e);
 })
