e888df26fc437bb01fe187f8b630f7ca73f9d847
Francesca4242 <32270681+Francesca4242@users.noreply.github.com>
2018-05-27 15:17:55 +0100

    text box and style changes

--- game2.js
+++ game2.js
@@ -1,6 +1,26 @@
+
+
+
+
+
+$('#stage2').hide();
+$('#stage3').hide();
+
 $('#takeoff').click(function() {
 
-	$('#stage1').hide();
-  startgame();
+	$('#stage1').hide(1000);
+	$('#stage2').show(1000);
 
 });
+
+$('#start').click(function(){
+	startgame();
+	
+})
+
+$('#takeoff2').click(function() {
+
+	$('#stage3').hide(1000);
+  startgame();
+});
+
